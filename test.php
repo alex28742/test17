ef433b4ede317c406bc04563c8465dd64c5ee1d0
alex28742 <allex.box@yandex.ru>
2016-11-12 00:15:52 +0300

    third commit

--- test.php
+++ test.php
@@ -31,5 +31,4 @@ foreach ($newArray as $key => $value) {
 }
 
 // ok
-// новое изменение файла
-// еще одно изменение файла
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
