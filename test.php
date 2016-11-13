5eb25b1397bb1977d04cb8615210d05ae6baced0
alex28742 <allex.box@yandex.ru>
2016-11-13 20:12:37 +0300

    text hello world

--- test.php
+++ test.php
@@ -30,5 +30,4 @@ foreach ($newArray as $key => $value) {
 	echo "$key => $value . \n";
 }
 
-// ok
-Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+echo "hello world!";
